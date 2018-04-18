2c8fb229f5d1a6e4e1fcf5efd6ea21e89227f302
PrimRose-MarkTuan <37031436+PrimRose-MarkTuan@users.noreply.github.com>
2018-04-18 17:13:19 +0800

    Create TEST.cpp

---
+++ chapter16/ex16.2/TEST.cpp
@@ -0,0 +1,12 @@
+#include "Compare.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << compare(1, 0) << endl;
+	cout << compare(1.2, 2.5) << endl;
+	cout << compare(1.2, 1.2) << endl;
+	cout << compare("mom", "mark") << endl;
+}
